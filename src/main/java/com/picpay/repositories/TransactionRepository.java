ebc6d8c60a16b0726b0d8040f1e0128b63f05efa
Rodrigo Azevedo <razevedocosta@gmail.com>
2023-09-25 17:44:42 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/picpay/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpay.repositories;
+
+import com.picpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
